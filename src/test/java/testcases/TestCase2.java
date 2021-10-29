4b17b4e7648f32242eaf4e37a273539489481386
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-29 15:45:00 +1200

    multiple failures with soft assertions

--- src/test/java/testcases/TestCase2.java
+++ src/test/java/testcases/TestCase2.java
@@ -2,16 +2,30 @@ package testcases;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class TestCase2 {
 	
 	@Test
 	public void validateTitles() {
 		
+		System.out.println("Beginning");
+		
 		String expected_title = "Yahoo.com";
 		String actual_title = "Gmail.com";
 		
-		Assert.assertEquals(actual_title, expected_title);
+		SoftAssert softAssert = new SoftAssert();
+		
+		System.out.println("Validating title");
+		softAssert.assertEquals(actual_title, expected_title);
+		
+		System.out.println("Validating img");
+		softAssert.assertEquals(true, false, "image errored");
+		
+		System.out.println("Ending");
+		
+		softAssert.assertAll();
+		
 	}
 
 }
