0bdeb54c41d5363323fad14e6bb8bfdd28cdfd38
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-31 11:22:28 +1200

    reading data using hashtables

--- src/test/java/TestNGParameterizationExcel.java
+++ src/test/java/TestNGParameterizationExcel.java
@@ -1,3 +1,5 @@
+import java.util.Hashtable;
+
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -6,11 +8,14 @@ public class TestNGParameterizationExcel {
 	public static ExcelReader excel = null;
 	
 	@Test(dataProvider = "getData")
-	public void testData(String username, String password, String is_correct) {
+	public void testData(Hashtable<String, String> data) {
 		
-		System.out.println(username + "---" + password + "---" + is_correct);
+//		System.out.println(username + "---" + password + "---" + is_correct);
+		System.out.println(data.get("username") + "---" + data.get("password") + "---" + data.get("is_correct") + "---" + data.get("age"));
 		
 	}
+	
+	// Hashtable
 	
 	@DataProvider
 	public static Object[][] getData() {
@@ -28,13 +33,20 @@ public class TestNGParameterizationExcel {
 		System.out.println(rows);
 		System.out.println(cols);
 		
-		Object[][] data = new Object[rows - 1][cols];
+		Object[][] data = new Object[rows - 1][1];
+		
+		Hashtable<String, String> table = null;
 		
 		for(int rowNum = 2; rowNum <= rows; rowNum++) {
 			
+			table = new Hashtable<String, String>();
+			
 			for(int colNum = 0; colNum < cols; colNum++) {
 				
-				data[rowNum - 2][colNum] = excel.getCellData(sheetName, colNum, rowNum);
+//				data[rowNum - 2][colNum] = excel.getCellData(sheetName, colNum, rowNum);
+				
+				table.put(excel.getCellData(sheetName, colNum, 1), excel.getCellData(sheetName, colNum, rowNum));
+				data[rowNum - 2][0] = table;
 				
 			}
 			
