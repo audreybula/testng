4613c96c2b61fe18b2faf4b8ef9345a92050e350
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-31 01:10:38 +1200

    reading excel dynamic

--- src/test/java/ReadingExcel.java
+++ src/test/java/ReadingExcel.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -19,10 +20,30 @@ public class ReadingExcel {
 		Workbook workbook = WorkbookFactory.create(fi);
 		
 		Sheet sheet0 = workbook.getSheetAt(0);
-		Row row0 = sheet0.getRow(0);
-		Cell cellA = row0.getCell(0);
 		
-		System.out.println(cellA);
+		for(Row row: sheet0) {
+			for(Cell cell: row) {
+				switch(cell.getCellType()) {
+				case STRING:
+					System.out.println(cell.getStringCellValue() + "\t");
+					break;
+				case NUMERIC:
+					System.out.println(cell.getStringCellValue() + "\t");
+					break;
+				case BLANK:
+					System.out.println("Blank cell" + "\t");
+					break;
+				default:
+					break;
+				}
+			}
+			System.out.println();
+		}
+		
+//		Row row0 = sheet0.getRow(0);
+//		Cell cellA = row0.getCell(0);
+//		
+//		System.out.println(cellA);
 		
 		fi.close();
 
