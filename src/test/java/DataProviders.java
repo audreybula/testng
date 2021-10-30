f91ca482839a1c9ed3b060e89115dad1bbdb0082
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-31 11:55:53 +1200

    single data provider change parameter number on runtime

--- src/test/java/DataProviders.java
+++ src/test/java/DataProviders.java
@@ -1,18 +1,37 @@
+import java.lang.reflect.Method;
+
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
 
 public class DataProviders {
 	
 	@DataProvider(name="dp1")
-	public static Object[][] getData() {
+	public static Object[][] getData(Method m) {
 		
-		Object[][] data = new Object[2][2];
+		Object[][] data = null;
 		
-		data[0][0] = "raman";
-		data[0][1] = "drfvfdb";
-		
-		data[1][0] = "rahul";
-		data[1][1] = "erdvsd";
+		if(m.getName().equals("testLogin")) {
+			
+			data = new Object[2][2];
+			
+			data[0][0] = "raman";
+			data[0][1] = "drfvfdb";
+			
+			data[1][0] = "rahul";
+			data[1][1] = "erdvsd";
+			
+		} else if(m.getName().equals("testUserReg")) {
+			
+			data = new Object[2][3];
+			
+			data[0][0] = "raman";
+			data[0][1] = "drfvfdb";
+			data[0][2] = "mail1";
+			
+			data[1][0] = "rahul";
+			data[1][1] = "erdvsd";
+			data[1][2] = "mail2";
+			
+		}
 		
 		return data;
 		
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,13 +3,13 @@ import org.testng.annotations.Test;
 public class LoginTest {
 	
 	@Test(dataProviderClass = DataProviders.class, dataProvider = "dp1")
-	public void testData(String username, String password) {
+	public void testLogin(String username, String password) {
 		
 		System.out.println(username + "---" + password);
 		
 	}
 	
-	@Test(dataProviderClass = DataProviders.class, dataProvider = "dp2")
+	@Test(dataProviderClass = DataProviders.class, dataProvider = "dp1")
 	public void testUserReg(String fname, String lname, String email) {
 		
 		System.out.println(fname + "---" + lname + "---" + email);
