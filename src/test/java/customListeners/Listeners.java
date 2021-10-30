b39a01d0b12d37af1e48458572325dd84ad12eb0
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-30 23:11:29 +1200

    reportng with html output

--- src/test/java/customListeners/Listeners.java
+++ src/test/java/customListeners/Listeners.java
@@ -2,19 +2,22 @@ package customListeners;
 
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.Reporter;
 
 public class Listeners implements ITestListener {
-	
-public void onTestSuccess(ITestResult result) {
-		
+
+	public void onTestSuccess(ITestResult result) {
+
 		System.out.println("Capturing Screenshot for passed test: " + result.getName());
-		
+
 	}
-	
+
 	public void onTestFailure(ITestResult result) {
-		
-		System.out.println("Capturing Screenshot for failed test: " + result.getName());
-		
+
+		System.setProperty("org.uncommons.reportng.escape-output", "false");
+		Reporter.log(
+				"<a href=\"/Users/admin/Desktop/dev/selenium/testng/src/screenshot/random.jpeg\">Screenshot Link</a>");
+
 	}
 
 }
